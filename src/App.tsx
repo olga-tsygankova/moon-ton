19f90a072a0406c1470c88e3bc01d10ec727b135
nickelink <nickelink@github.com>
2024-06-11 06:12:46 +0300

    (Join): рефакторинг и пробелы

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import './App.css';
-import {Main} from "./components/Main/Main";
-import {Mission} from "./components/Mission/Mission";
-import {Product} from "./components/Product";
-import {ForUsers} from "./components/ForUsers";
-import {Join} from "./components/Join";
-import {Footer} from "./components/Footer";
-import {Advantages} from "./components/Advantages";
-import {Roadmap} from "./components/Roadmap";
-import {Token} from "./components/Token";
-import {LineFooter} from "./ui/svg/LineFooter";
-//<Mission/>
-//             <Product/>
-//             <ForUsers/>
-//             <Advantages/>
-//             <Roadmap/>
-//             <Token/>
-//             <Join/>
-//             <Footer/>
+import { Main, Join } from './components';
+import { Mission } from './components/Mission/Mission';
+import { Product } from './components/Product';
+import { ForUsers } from './components/ForUsers';
+import { Footer } from './components/Footer';
+import { Advantages } from './components/Advantages';
+import { Roadmap } from './components/Roadmap';
+import { Token } from './components/Token';
+import { LineFooter } from './ui/svg/LineFooter';
+
 
 function App() {
-    return (
-        <div className="App">
-            <Main/>
-
-        </div>
-    );
+  return (
+    <div className="App">
+      <Main/>
+      {/*<Mission/>*/}
+      {/*<Product/>*/}
+      {/*<ForUsers/>*/}
+      {/*<Advantages/>*/}
+      {/*<Roadmap/>*/}
+      {/*<Token/>*/}
+      {/*<Join/>*/}
+      {/*<Footer/>*/}
+    </div>
+  );
 }
 
 export default App;
