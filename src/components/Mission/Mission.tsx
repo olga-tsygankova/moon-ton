f13999ba9a58cd744c0b70e5599671bfe2185537
Olga Tsygankova <tsygankova@perx.ru>
2024-06-12 05:30:27 +0300

    refactor css

--- src/components/Mission/Mission.tsx
+++ src/components/Mission/Mission.tsx
@@ -1,10 +1,11 @@
-import './Mission.css';
-import { useEffect, useRef, useState } from 'react';
-
+import "./Mission.css";
+import { useEffect, useRef, useState } from "react";
 
 export const Mission = () => {
   const missionTextRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+
+  //TODO: разбитую на буквы строку нужно после отработки анимации преобразовать обратно в строчу чтобы был перенос текста
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -14,9 +15,9 @@ export const Mission = () => {
         });
       },
       {
-        rootMargin: '0px',
-        threshold: 0.5 // Триггерить событие, когда элемент виден на 50% или больше
-      }
+        rootMargin: "0px",
+        threshold: 0.5, // Триггерить событие, когда элемент виден на 50% или больше
+      },
     );
 
     if (missionTextRef.current) {
@@ -32,28 +33,27 @@ export const Mission = () => {
 
   const renderMissionText = () => {
     const text =
-      'Our mission is to ensure seamless, secure, and efficient omnichain interaction between ton and other blockchain ecosystems';
-    return text.split('').map((char, index) => (
+      "Our mission is to ensure seamless, secure, and efficient omnichain interaction between ton and other blockchain ecosystems";
+    return text.split("").map((char, index) => (
       <span
         key={index}
         style={{
-          display: 'inline-block',
-          animation: `${isVisible ? 'colorChangeOnVisible' : 'colorChangePerLetter'} ${
+          display: "inline-block",
+          animation: `${isVisible ? "colorChangeOnVisible" : "colorChangePerLetter"} ${
             0.1 * (index + 1)
           }s ease-in-out forwards`,
         }}
       >
-      {char === ' ' ? '\u00A0' : char}
-    </span>
+        {char === " " ? "\u00A0" : char}
+      </span>
     ));
   };
-
 
   return (
     <div className="mission">
       <p
         ref={missionTextRef}
-        className={`mission-text ${isVisible ? 'visible' : ''}`}
+        className={`mission-text ${isVisible ? "visible" : ""}`}
       >
         {renderMissionText()}
       </p>
