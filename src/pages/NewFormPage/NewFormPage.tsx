516662977033849ca608fa02f5055cbb5521616e
nickelink <nickelink@github.com>
2024-07-02 01:30:50 +0300

    (Form): Добавит форматирование сообщения

--- src/pages/NewFormPage/NewFormPage.tsx
+++ src/pages/NewFormPage/NewFormPage.tsx
@@ -3,30 +3,22 @@ import { useForm } from 'react-hook-form';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Footer, Header } from '../MainPage';
-import { Form, FormRadioGroup, FormTextArea, FormTextInput } from '../../components';
 import { SubmitButton } from '../../ui';
 import { sendMessage } from '../../api/telegram';
-import { FormBlock } from './FormBlock';
-import { UploadAsset } from './UploadAsset';
+import { FormBody } from './FormBody';
 import type { IContactForm } from './models';
+import { prepareMessage } from './utils';
 
 import './NewFormPage.css';
-
-const chainOptions = [
-  { value: 'Ethereum', label: 'Ethereum' },
-  { value: 'Solana', label: 'Solana' },
-  { value: 'TON', label: 'TON' },
-];
 
 export const NewFormPage = () => {
   const methods = useForm<IContactForm>();
   const [isFetching, setIsFetching] = useState(false);
 
-  const handleSendData = async (data: Record<string, any>) => {
+  const handleSendData = async (data: IContactForm) => {
     try {
       setIsFetching(true);
-      const message = JSON.stringify(data);
-      await sendMessage(message);
+      await sendMessage(prepareMessage(data));
     } catch (error) {
       console.log(error);
     } finally {
@@ -39,7 +31,6 @@ export const NewFormPage = () => {
       (data) => {
         console.log('VALID');
         console.log(data);
-        console.log(JSON.stringify(data));
         handleSendData(data);
       },
       (data) => {
@@ -53,56 +44,15 @@ export const NewFormPage = () => {
     <div className="new-form-page">
       <Header />
       <div className="form-all-container">
-        <Form methods={methods}>
-          <FormBlock title="About Project">
-            <FormTextInput fieldName="projectName" title="Project Name*" placeholder="Name" />
-            <FormTextInput fieldName="tokenSymbol" title="Token Symbol or Ticker*" placeholder="$MOON" />
-            <FormTextArea
-              fieldName="projectDescription"
-              title="Project Description"
-              placeholder="Describe your project"
-            />
-          </FormBlock>
-          <FormBlock title="Contact">
-            <FormTextInput fieldName="name" title="Name*" placeholder="Name" />
-            <FormTextInput fieldName="email" title="Email" placeholder="email@gmail.com" />
-            <FormTextInput fieldName="telegramLink" title="Telegram Link*" placeholder="@username" />
-          </FormBlock>
-          <FormBlock title="Project Link">
-            <FormTextInput
-              fieldName="projectTwitterLink"
-              title="Project Twitter Link*"
-              placeholder="https:// twitter.com/moonton"
-            />
-            <FormTextInput
-              fieldName="projectTelegramLink"
-              title="Project Telegram Link*"
-              placeholder="https://t.me/moonton"
-            />
-            <FormTextInput
-              fieldName="projectWebsite"
-              title="Project Website*"
-              placeholder="https://moonton.io"
-            />
-          </FormBlock>
-          <FormBlock title="Other">
-            <FormRadioGroup fieldName="chain" options={chainOptions} title="Destination chain*" />
-            <FormTextInput
-              fieldName="liquidity"
-              title="How much liquidity will you be adding in the pool?*"
-              placeholder="how much?"
-            />
-            <UploadAsset />
-          </FormBlock>
-          <div>
-            <SubmitButton
-              onClick={handleSubmit}
-              disabled={isFetching}
-            >
-              Submit
-            </SubmitButton>
-          </div>
-        </Form>
+        <FormBody methods={methods} />
+        <div>
+          <SubmitButton
+            onClick={handleSubmit}
+            disabled={isFetching}
+          >
+            Submit
+          </SubmitButton>
+        </div>
       </div>
 
       {/*<Footer />*/}
