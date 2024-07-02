705fad85fe0e42ed7d362b4783090f076c75a5e7
Olga Tsygankova <89998178494@mail.ru>
2024-07-02 13:18:47 +0300

    заменила эмодзи

--- src/pages/NewFormPage/utils.ts
+++ src/pages/NewFormPage/utils.ts
@@ -1,21 +1,21 @@
-import type { IContactForm } from './models';
+import type { IContactForm } from "./models";
 
 export const prepareMessage = (data: IContactForm) => {
   const message = `<b>About Project:</b>
-  🗿 Project Name: ${data.projectName ?? 'NO_DATA'}
-  🅱️ Token Ticker: ${data.tokenSymbol ?? 'NO_DATA'}
-  🔜 Project Description: ${data.projectName ?? 'NO_DATA'}
+  🔥 Project Name: ${data.projectName ?? "NO_DATA"}
+  🏷 Token Ticker: ${data.tokenSymbol ?? "NO_DATA"}
+  📑 Project Description: ${data.projectName ?? "NO_DATA"}
   <b>Contact Info:</b>
-  🤹 Name: ${data.name ?? 'NO_DATA'}
-  📧 Email: ${data.email ?? 'NO_DATA'}
-  📱 Telegram: ${data.telegramLink ?? 'NO_DATA'}
+  👦🏼 Name: ${data.name ?? "NO_DATA"}
+  📧 Email: ${data.email ?? "NO_DATA"}
+  📱 Telegram: ${data.telegramLink ?? "NO_DATA"}
   <b>Contact Link:</b>
-  🦤 Twitter: ${data.projectTwitterLink ?? 'NO_DATA'}
-  👻 Telegram: ${data.projectTelegramLink ?? 'NO_DATA'}
-  🖥 Website: ${data.projectWebsite ?? 'NO_DATA'}
+  🕊 Twitter: ${data.projectTwitterLink ?? "NO_DATA"}
+  📱 Telegram: ${data.projectTelegramLink ?? "NO_DATA"}
+  🖥 Website: ${data.projectWebsite ?? "NO_DATA"}
   <b>Other:</b>
-  ⛓ Destination chain: ${data.chain ?? 'NO_DATA'}
-  📊 Liquidity volume: ${data.liquidity ?? 'NO_DATA'}
+  ⛓ Destination chain: ${data.chain ?? "NO_DATA"}
+  📊 Liquidity volume: ${data.liquidity ?? "NO_DATA"}
   📚 Files: TODO
   `;
 
